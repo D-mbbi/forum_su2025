d4c916e25e9adb07c135a83502ce81922b54bc6c
D-mbbi <doelmbbi@gmail.com>
2025-05-23 18:58:35 +0200

    changeStatus

--- organiz_asso/client/src/components/UserProfile.jsx
+++ organiz_asso/client/src/components/UserProfile.jsx
@@ -8,7 +8,21 @@ function UserProfile(){
 
     const { id } = useParams(); // récupère le userID dans l'URL
 
+    const [user, setUser] = useState();
     const [userMessages, setUserMessages] = useState([]);
+    const [currentUser, setCurrentUser] = useState();
+
+    useEffect(() => {
+        axios.get('/api/user/me', { withCredentials: true })
+        .then(res => setCurrentUser(res.data.user))
+        .catch(err => console.error(err));
+    }, [user]);
+
+    useEffect(() => {
+        axios.get('/api/user/getUser', { withCredentials: true , params : {username : id}})
+            .then(res => setUser(res.data.user))
+            .catch(err => console.error(err));
+    }, [user]);
 
     useEffect(() => {
         axios.get(`/api/post/getUserPost/${id}`, {withCredentials : true})
@@ -21,11 +35,30 @@ function UserProfile(){
         .catch(err => console.error(err))
     },[id])
 
+    const toggleAdminStatus = () => {
+  axios.put(`/api/user/${id}/setStatus`, {
+    admin: user.admin ? false : true
+  }, { withCredentials: true })
+    .then(res => {
+      setUser(res.data.user);
+    })
+    .catch(err => console.error(err));
+};
+
+    if(!currentUser || !user){
+        return <h1>Chargement...</h1>
+    }
+
     return (
         <div className="user-profile">
             <NavBar />
             <div className="container">
                 <h1 className="UserProfileHeader">Profil de {id}</h1>
+                {currentUser.admin && currentUser.username !== id && (
+  <button onClick={toggleAdminStatus} className="admin-toggle-btn">
+    {user.admin ? 'Révoquer les droits admin' : 'Promouvoir en admin'}
+  </button>
+)}
                 <MessagesList message={userMessages} />
             </div>
         </div>
