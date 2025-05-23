75696e8d234bf2e8acbfbcebb95a3428def5e7b8
eliastrk <eliasturki003@gmail.com>
2025-05-23 14:37:40 +0200

    mtn les messages les plus recents tout en haut

--- organiz_asso/client/src/components/MessagesList.jsx
+++ organiz_asso/client/src/components/MessagesList.jsx
@@ -6,14 +6,14 @@ function MessagesList(props) {
 
     return (
         <div className="messagesLists">
-            {message.length == 0 ? 'Aucun message' : message.map((msg) => (
+            {message.length == 0 ? 'Aucun message' : [...message].reverse().map((msg) => (
                 !msg.answeredPostID ?
                 <Message
                     key={msg._id}
                     msgID={msg._id}
                     userID={msg.userID}
                     contenu={msg.content}
-                    date={  `${(new Date(msg.date)).getDate().toString().padStart(2, '0')}/${((new Date(msg.date)).getMonth()+1).toString().padStart(2, '0')}/${(new Date(msg.date)).getFullYear().toString()}`   }
+                    date={`${(new Date(msg.date)).getDate().toString().padStart(2, '0')}/${((new Date(msg.date)).getMonth()+1).toString().padStart(2, '0')}/${(new Date(msg.date)).getFullYear().toString()}`   }
                     heure={`${(new Date(msg.date)).getHours().toString().padStart(2, '0')}:${(new Date(msg.date)).getMinutes().toString().padStart(2, '0')}`}
                     nbLikes={msg.likes || 0}
                     comments={msg.comments || []}
